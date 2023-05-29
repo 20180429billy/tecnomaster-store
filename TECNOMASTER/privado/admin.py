0ae795f6799d2d143cd2668eb16e519c114bf08b
DiegoParada9V <125921935+DiegoParada9V@users.noreply.github.com>
2023-05-29 15:04:03 -0600

    cambios

--- TECNOMASTER/privado/admin.py
+++ TECNOMASTER/privado/admin.py
@@ -2,17 +2,20 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Categoria)
-admin.site.register(EstadoCliente)
-admin.site.register(Cliente)
-admin.site.register(EstadoUsuario)
-admin.site.register(TipoUsuario)
-admin.site.register(Usuario)
-admin.site.register(Marcas)
-admin.site.register(Producto)
-admin.site.register(ImagenProducto)
-admin.site.register(EstadoProducto)
-admin.site.register(EstadoPedido)
-admin.site.register(Pedido)
-admin.site.register(Valoraciones)
-admin.site.register(EstadoValoraciones)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
